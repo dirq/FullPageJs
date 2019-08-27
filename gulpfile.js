f7f42a614cb249ac0b9eb1b574f70315acdfa72d
Dirk <dwatkins@dirq.net>
2019-08-27 16:28:21 -0500

    server with browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,17 @@ var rename = require('gulp-rename');
 var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
 var minifyCss = require('gulp-clean-css');
+var browserSync = require('browser-sync').create();
+
+// ==============================================
+// Directory path variables
+// ==============================================
+const config = {
+	appDirectory: 'src/',
+	tempDirectory: 'temp/',
+    distributionDirectory: 'dist/',
+    examplesDirectory: 'examples/'
+};
 
 gulp.task('css', function(done) {
     gulp.src('./src/fullpage.css')
@@ -66,3 +77,26 @@ gulp.task('extensions', function(done) {
 });
 
 gulp.task('default', gulp.parallel('css', 'js'));
+
+
+const startServer =function(){
+    browserSync.init({
+		server: {
+			baseDir: ['./examples','./'],
+			port: 3000,
+			directory: true
+		}
+	});
+
+	gulp.watch([config.appDirectory + '**/*.html', config.examplesDirectory + '**/*.html', config.examplesDirectory + 'imgs/**/*.*']).on('change', browserSync.reload);
+
+	gulp.watch(config.appDirectory + '*.css', gulp.parallel('css'));
+
+	gulp.watch(config.appDirectory + '*.js', gulp.parallel('js'));
+
+	// protip: stop old version of gulp watch from running when you modify the gulpfile
+	gulp.watch('gulpfile.js').on('change', () => process.exit(0));
+};
+
+
+gulp.task('serve', gulp.series('css', 'js', startServer));
